47d6072bd784a97140b6ba2585c9229df63a2638
Boluwatife-AJB <56480003+Boluwatife-AJB@users.noreply.github.com>
2022-08-30 13:41:24 +0100

    navbar changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Box } from '@mui/material';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Airbnb Clone</h1>
-    </div>
-  );
-}
+import {
+  Navbar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+} from './components';
+
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: '#222' }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
 
 export default App;
